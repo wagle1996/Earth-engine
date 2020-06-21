f0f8581812cf1db86fdedebac423880320c9622c
Nimisha Wagle <wagle1996@gmail.com>
2020-06-21 23:18:24 +0545

    Update monthly_ndvi.js

--- monthly_ndvi.js
+++ monthly_ndvi.js
@@ -12,9 +12,10 @@ var mean = reference.mean();
 // Compute anomalies by subtracting the 2001-2010 mean from each image in a
 // collection of 2011-2014 images. Copy the date metadata over to the
 // computed anomaly images in the new collection.
-var series = collection.filterDate('2011-01-01', '2014-12-31').map(function(image) {return image.subtract(mean).set('system:time_start', image.get('system:time_start'));
-});
-
+var series = collection.filterDate('2012-01-01', '2014-12-31')
+//.map(function(image) {return image.subtract(mean).set('system:time_start', image.get('system:time_start'));
+//});
+print (series);
 // Display cumulative anomalies.
 Map.setCenter(84, 27, 5);
 Map.addLayer(series.sum().clip(Da),
@@ -33,44 +34,62 @@ var first = ee.List([
 
 // This is a function to pass to Iterate().
 // As anomaly images are computed, add them to the list.
-var accumulate = function(image, list) {
-  // Get the latest cumulative anomaly image from the end of the list with
-  // get(-1).  Since the type of the list argument to the function is unknown,
-  // it needs to be cast to a List.  Since the return type of get() is unknown,
-  // cast it to Image.
-  var previous = ee.Image(ee.List(list).get(-1));
-  // Add the current anomaly to make a new cumulative anomaly image.
-  var added = image.add(previous)
-    // Propagate metadata to the new image.
-    .set('system:time_start', image.get('system:time_start')).float();
-  // Return the list with the cumulative anomaly inserted.
-  return ee.List(list).add(added);
+// var accumulate = function(image, list) {
+//   // Get the latest cumulative anomaly image from the end of the list with
+//   // get(-1).  Since the type of the list argument to the function is unknown,
+//   // it needs to be cast to a List.  Since the return type of get() is unknown,
+//   // cast it to Image.
+//   var previous = ee.Image(ee.List(list).get(-1));
+//   // Add the current anomaly to make a new cumulative anomaly image.
+//   var added = image.add(previous)
+//     // Propagate metadata to the new image.
+//     .set('system:time_start', image.get('system:time_start'));
+//   // Return the list with the cumulative anomaly inserted.
+//   return ee.List(list).add(added);
+// };
+
+// Comment
+var tofloat=function(image){
+  return image.toFloat()
 };
 
+//print (accumulate);
 // };
+
 // Create an ImageCollection of cumulative anomaly images by iterating.
 // Since the return type of iterate is unknown, it needs to be cast to a List.
 
 
-var cumulative = ee.ImageCollection(ee.List(series.iterate(accumulate, first)));
+// var cumulative = ee.ImageCollection(ee.List(series.iterate(accumulate, first)));
+//var cum= tofloat(cumulative)
+// print (cumulative);
+//print (cum);
+// Scale the NDVI with fac 0.0001
+var scaleNDVI = function(img){
+  return img.multiply(0.0001)
+}
 
-print (cumulative);
-
+// scale every image in the collection
+var cum = series.map(scaleNDVI);
+print (cum)
 var counter = 0;
+var collectionList = cum.toList(cum.size())
+var collectionSize = collectionList.size().getInfo()
 
 // loop through the year
-for (var y = 2011; y < 2015 ; y++) {
+for (var y = 2012; y < 2015 ; y++) {
   // loop through the months
-  for (var m = 1; m < 12 ; m++) {
-  
+  for (var m = 1; m <= 12 ; m++) {
   // get the image
-  var img = ee.Image(cumulative.toList(1, counter).get(0));
- 
-  // store the image
+  var img = ee.Image(collectionList.get(m));
+  print (img)
+
+// store the image
   Export.image.toDrive({
-       image: img,
+       image: img.toFloat(),
        description: y.toString() + m.toString(),
        scale: 1000,
     region: Da
  });
 }}
+
